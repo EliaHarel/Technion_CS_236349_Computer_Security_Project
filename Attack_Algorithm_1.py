f9da80717aae370cee42e1779d260df3592f648b
DanielYacoby <64319684+DanielYacoby@users.noreply.github.com>
2020-05-01 15:41:35 +0300

    starting the attack inself

--- Attack_Algorithm_1.py
+++ Attack_Algorithm_1.py
@@ -113,6 +113,48 @@ for level in range(15):
             mat[level][key]= matrix_product(mat[level-2][subkey_left], mat[2][subkey_right])
 
 
+# out s5 after permutation - 3, 8, 14, 25
+# out s1 after permutation - 9, 17, 23, 31
+# mask for plaintext - out s5 || out s1 - 3, 8, 14, 25, 41, 49, 55, 63
+# mask for ciphertext - out s1 || out s5 - 9, 17, 23, 31, 35, 40, 46, 57
+
+mask_for_plain = [3, 8, 14, 25, 41, 49, 55, 63]
+mask_for_cipher = [9, 17, 23, 31, 35, 40, 46, 57]
+
+
+# returns a substring which contains bits from specific places in str
+def get_sub_input(list, str):
+    sub_str = []
+    for i in range(list.len()):
+        sub_str += str[list[i]]
+
+
+# reads the next pair of inputs and return the relevant bits from them
+# the relevant bits are the outputs bits of S1, S5
+def get_next_sub_input(index):
+    (plain, cipher) = get_next_input_from_file(index)
+    binary_plain = format(plain, '64b')
+    binary_cipher = format(cipher, '64b')
+    sub_plain = get_sub_input(mask_for_plain, binary_plain)
+    sub_cipher = get_sub_input(mask_for_cipher, binary_cipher)
+    return (int(sub_plain), int(sub_cipher))
+
+
+num_of_inputs  #  from the file
+
+mat_summing = [[0 for j in range(size)]for i in range(size)]
+sum_for_plaintext =[0 for i in range(size)]
+
+for i in range(num_of_inputs):
+    (plain, cipher) = get_next_sub_input(i)  # 8 bits of the i-th plaintext and ciphertext as decimal number
+    mat_summing[plain][cipher] += 1
+    sum_for_plaintext[plain] += 1
+
+mat_probabilities = [[0 for j in range(size)]for i in range(size)]
+for plain in range(size):
+    for cipher in range(size):
+        mat_probabilities[plain][cipher] = mat_summing / sum_for_plaintext[plain]  # conditional probability
+
 
 #######################################################
 #                        Tests

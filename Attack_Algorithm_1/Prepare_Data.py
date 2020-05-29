579adbaf0b71fef4bea46c0263c61519ea94202a
EliaHarel <EliaHarel@users.noreply.github.com>
2020-05-29 12:56:41 +0300

    test_data

--- Attack_Algorithm_1/Prepare_Data.py
+++ Attack_Algorithm_1/Prepare_Data.py
@@ -1,44 +1,44 @@
 import subprocess
 import os
 
-plain_file = './../Plain Texts/5_plains.txt'
+
+file_prefix = "2500_plains_"
+num_of_files = 20
+
 rounds = "4"
 key = "0100100100100100100100001001001100001001001101001000101000100100"
 Mode = "1"  # 0 - Decrypt, 1 - Encrypt
 # the number of plain texts and cipher texts is determined by the input file
 
-output_file_name = "Data.txt"
-# c_code_file = "./../DES_C/cmake-build-debug/DES_C.exe"
+
+file_path_prefix = "./../Plain Texts/" + file_prefix
+output_path_prefix = "Data" + file_prefix
+
 c_code_file = os.pardir + "\DES_C\cmake-build-debug\DES_C.exe"
 
-
-plain_object = open(plain_file, "r")
-data_object = open(output_file_name, "w")
-data_object.write("rounds: " + rounds + " key: " + key + "\n")
-
-
-for plaintext in plain_object.readlines():
-	key = str(key)
-	# print(key)
-	rounds = str(rounds)
-	# print(rounds)
-	mode = str(Mode)
-	# print(mode)
-	plain =str(plaintext)
-	# print(plain)
-	command = [c_code_file, key, rounds, mode, plain]
-	ciphertext = subprocess.check_output(command ,shell=True)
-	plain_cipher_line = str(plaintext[:-1]) + " " + str(ciphertext)[2:-1] + '\n'
-	data_object.write(plain_cipher_line)
-
-plain_object.close()
-data_object.close()
-
-# 0100010010000111001100001110100101101100001010011011110101010111
-# 0000101001110011001110000111011110011001100010101011111011010110
+for files_index in range(num_of_files):
+	file_path = file_path_prefix + str(files_index) + ".txt"
+	# plain_file = plain_file_prefix + str(files_index)
+	output_path = output_path_prefix + str(files_index) + ".txt"
+	# output_file_name = output_path_prefix +  + str(files_index)
+	input_file = open(file_path, "r")
+	output_file = open(output_path, "w")
+	output_file.write("rounds: " + rounds + " key: " + key + "\n")
 
 
-# >>> from subprocess import check_output
-# >>> foo = check_output('./th', shell=True)
-# >>> foo
-# '17.9 51.0'
+	for plaintext in input_file.readlines():
+		key = str(key)
+		# print(key)
+		rounds = str(rounds)
+		# print(rounds)
+		mode = str(Mode)
+		# print(mode)
+		plain =str(plaintext)
+		# print(plain)
+		command = [c_code_file, key, rounds, mode, plain]
+		ciphertext = subprocess.check_output(command ,shell=True)
+		plain_cipher_line = str(plaintext[:-1]) + " " + str(ciphertext)[2:-1] + '\n'
+		output_file.write(plain_cipher_line)
+	
+	input_file.close()
+	output_file.close()
--- Plain Texts/Plain Text Generator.py
+++ Plain Texts/Plain Text Generator.py
@@ -1,25 +1,23 @@
 import random
 from pathlib import Path
 
-num_of_plaintexts = 5
-
-
-new_file_name = str(num_of_plaintexts)+"_plains.txt"
-Path(new_file_name).touch()
-
-file = open(new_file_name, "w") 
-
-
-for i in range(num_of_plaintexts):
-	plain = ""
-	for j in range(8):
-		x = random.randint(0,256)
-		x = format(x, '08b')
-		plain+=str(x)
-	file.write(plain)
-	file.write('\n')
-
-file.close()
+num_of_plaintexts = 2500
+num_of_files = 20
+for file_index in range(num_of_files):
+	new_file_name = str(num_of_plaintexts)+"_plains_"+str(file_index)+".txt"
+	
+	Path(new_file_name).touch()
+	file = open(new_file_name, "w") 
+	
+	for i in range(num_of_plaintexts):
+		plain = ""
+		for j in range(8):
+			x = random.randint(0,256)
+			x = format(x, '08b')
+			plain+=str(x)
+		file.write(plain)
+		file.write('\n')
+	file.close()
 
 
 

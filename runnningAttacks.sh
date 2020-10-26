7fdf88699871ca6f4adebd28f2eebfa7590bdd63
EliaHarel <EliaHarel@users.noreply.github.com>
2020-10-26 21:44:39 +0200

    update attack script

---
+++ runnningAttacks.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# ./AttackAll attack_num rounds pairs iterations ~/resutls
+
+# ./AttackAll 1 6 20000 50 ./results/Attack1
+# ./AttackAll 1 6 50000 50 ./results/Attack1
+# ./AttackAll 1 6 100000 50 ./results/Attack1
+# ./AttackAll 1 8 100000 50 ./results/Attack1
+# ./AttackAll 1 8 200000 50 ./results/Attack1
+# ./AttackAll 1 8 500000 50 ./results/Attack1
+# ./AttackAll 1 10 500000 50 ./results/Attack1
+./AttackAll 1 10 800000 50 ./results/Attack1
+./AttackAll 1 10 1000000 50 ./results/Attack1
+./AttackAll 1 10 1200000 50 ./results/Attack1
+./AttackAll 1 12 800000 50 ./results/Attack1
+./AttackAll 1 12 1000000 50 ./results/Attack1
+./AttackAll 1 12 1200000 50 ./results/Attack1
+./AttackAll 1 12 1500000 50 ./results/Attack1

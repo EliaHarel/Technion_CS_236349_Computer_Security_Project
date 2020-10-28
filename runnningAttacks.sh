93cc35eda5484f29703e3429593a3c56ad4b0b8f
EliaHarel <EliaHarel@users.noreply.github.com>
2020-10-28 17:18:13 +0200

    Update runnningAttacks.sh

--- runnningAttacks.sh
+++ runnningAttacks.sh
@@ -18,8 +18,8 @@ arr_10_rounds=(20000 50000 100000 200000)
 arr_12_rounds=(20000 50000 100000 200000)
 arr_14_rounds=(100000 200000 500000)
 
-for pairs in ${arr_6_rounds[@]}; do; echo ./AttackAll ${attack_num} 6 ${pairs} $iterations ; done
-for pairs in ${arr_8_rounds[@]}; do; echo ./AttackAll ${attack_num} 8 ${pairs} $iterations; done
-for pairs in ${arr_10_rounds[@]}; do; echo ./AttackAll ${attack_num} 10 ${pairs} $iterations; done
-for pairs in ${arr_12_rounds[@]}; do; echo ./AttackAll ${attack_num} 12 ${pairs} $iterations; done
-for pairs in ${arr_14_rounds[@]}; do; echo ./AttackAll ${attack_num} 14 ${pairs} $iterations; done
+for pairs in ${arr_6_rounds[@]}; do ./AttackAll ${attack_num} 6 ${pairs} $iterations ~/results/Attack2; done
+for pairs in ${arr_8_rounds[@]}; do ./AttackAll ${attack_num} 8 ${pairs} $iterations ~/results/Attack2; done
+for pairs in ${arr_10_rounds[@]}; do ./AttackAll ${attack_num} 10 ${pairs} $iterations ~/results/Attack2; done
+for pairs in ${arr_12_rounds[@]}; do ./AttackAll ${attack_num} 12 ${pairs} $iterations ~/results/Attack2; done
+for pairs in ${arr_14_rounds[@]}; do ./AttackAll ${attack_num} 14 ${pairs} $iterations ~/results/Attack2; done

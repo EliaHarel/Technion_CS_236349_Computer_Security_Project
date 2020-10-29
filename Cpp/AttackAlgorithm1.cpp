0cd67498f27af9f49b50c6c51a57b2fcd7fcabef
DanielYacoby <64319684+DanielYacoby@users.noreply.github.com>
2020-10-29 22:39:42 +0200

    attack1 update

--- Cpp/AttackAlgorithm1.cpp
+++ Cpp/AttackAlgorithm1.cpp
@@ -13,8 +13,11 @@
 
 
 namespace masks {
-    const std::vector<int> plain_mask{7, 13, 24, 2, 40, 48, 54, 62};
-    const std::vector<int> cipher_mask{39, 45, 56, 34, 8, 16, 22, 30};
+    const int plain_left_shift_mask[] = {24, 18, 7, 29 }; // {7, 13, 24, 2} 31-x
+    const int plain_right_shift_mask[] = {23, 15, 9, 1 }; // {40, 48, 54, 62} 63-x
+    const int cipher_left_shift_mask[] ={23, 15, 9, 1}; //{8, 16, 22, 30} 31-x
+    const int cipher_right_shift_mask[] ={24, 18, 7, 29}; //{39, 45, 56, 34} 63-x
+
     // key_mask[i] - the key bit we use in level i+1
     const std::vector<int> key_mask{27, 51, 3, 48, 38, 16, 6, 49, 45, 25, 13, 58, 44, 26, 12, 2};
 }
@@ -29,36 +32,63 @@ static std::string getSubInput(std::string& input, const std::vector<int>& mask)
     return sub_str;
 }
 
-static double calculateKeyDistance(int key, std::vector<std::vector<double>>& mat_summing,
+static int getSubInput(long long input, const int mask[4]){
+    int res =0;
+    int size = 4;
+    for(int i=0; i< size ; i++){
+        int shift_left = size -1 -i ;
+        res += ((input>> mask[i]) & 1) << shift_left;
+    }
+    return res;
+}
+
+static double calculateKeyDistance(int key, int mat_summing[matrix_size][matrix_size],
                                    int num_of_inputs, int num_of_rounds, vvvvd& pre_calculated_mat){
     double distance = 0;
     for(int i = 0; i < matrix_size; i ++){
         for(int j = 0; j < matrix_size; j ++){
             double part1 =
                     pre_calculated_mat.at(num_of_rounds).at(key).at(i).at(j) - 1.0/((double) matrix_size);
-            double part2 = mat_summing.at(i).at(j) - ((double) num_of_inputs/(matrix_size*matrix_size));
+            double part2 = mat_summing[i][j] - ((double) num_of_inputs)/(matrix_size*matrix_size);
             distance += part1*part2;
         }
     }
     return distance;
 }
 
+
+
 static std::pair<int, int> createPair(int num_of_rounds, std::string& binary_used_key){
-    std::pair<std::string, std::string> data;
-    data = getPlainCipherPair(num_of_rounds, binary_used_key);
-    auto str_sub_plain = getSubInput(data.first, plain_mask);
-    auto str_sub_cipher = getSubInput(data.second, cipher_mask);
-    return {binaryStrToInt(str_sub_plain), binaryStrToInt(str_sub_cipher)};
+    std::pair<std::string, std::string> plain_cipher_pair = getPlainCipherPair(num_of_rounds, binary_used_key);
+    std::string plain_left = plain_cipher_pair.first.substr(0, 32);
+    std::string plain_right = plain_cipher_pair.first.substr(32, 32);
+    std::string cipher_left = plain_cipher_pair.second.substr(0, 32);
+    std::string cipher_right = plain_cipher_pair.second.substr(32, 32);
+
+    long long p_left = stoll(plain_left, nullptr, 2);
+    long long p_right = stoll(plain_right, nullptr, 2);
+    long long c_left = stoll(cipher_left, nullptr, 2);
+    long long c_right = stoll(cipher_right, nullptr, 2);
+
+    int plaintext_left = getSubInput(p_left, plain_left_shift_mask);
+    int plaintext_right = getSubInput(p_right, plain_right_shift_mask);
+    int ciphertext_right = getSubInput(c_left, cipher_left_shift_mask); // swap
+    int ciphertext_left = getSubInput(c_right, cipher_right_shift_mask); //swap
+
+
+    int plain = (plaintext_left << 4) + plaintext_right;
+    int cipher = (ciphertext_left << 4) + ciphertext_right;
+    return {plain, cipher};
 }
 
 int attackAlgorithm1(int num_of_rounds, int num_of_inputs, vvvvd& pre_calculated_mat){
 
     std::string binary_used_key;
     createBinText(binary_used_key);
-    std::vector<std::vector<double>> mat_summing(matrix_size, vd(matrix_size, 0));
+    static int mat_summing[matrix_size][matrix_size]= {0};
     for(int i = 0; i < num_of_inputs; i ++){
         std::pair<int, int> plain_cipher_pair = createPair(num_of_rounds, binary_used_key);
-        mat_summing.at(plain_cipher_pair.first).at(plain_cipher_pair.second) ++;
+        mat_summing[plain_cipher_pair.first][plain_cipher_pair.second]++;
     }
 
     std::string str_sub_used_key = getSubInput(binary_used_key, key_mask);
---
+++ Cpp/AttackAlgorithm2FewLevels.cpp
@@ -0,0 +1,122 @@
+//
+// Created by DanielYacoby on 23/10/2020.
+//
+
+#include "AttackAlgorithm2FewLevels.h"
+#include "AttackAlgorithm2.h"
+#include "DataUtils.h"
+
+
+#include <vector>
+#include <string>
+#include <cmath>
+
+const int s1_out_shift_mask[] = {23, 15, 9, 1}; //{8, 16, 22, 30};
+const int s5_out_shift_mask[] = {24, 18, 7, 29}; //{7, 13, 24, 2};
+const int NUM_OF_FIRST_LAST_KEYS = 4096;
+
+
+vi first_mask = {9, 50, 33, 59, 48, 16}; //starts at 0
+vvi last_mask = {{},
+                 {},
+                 {},
+                 {},
+                 {},
+                 {12, 22, 29, 44, 62, 61}, // 6 rounds
+                 {},
+                 {11, 53, 60, 12, 30, 29}, // 8 rounds
+                 {},
+                 {54, 29, 36, 19, 6,  5}, // 10 rounds
+                 {},
+                 {22, 60, 4,  54, 37, 36}, // 12 rounds
+                 {},
+                 {53, 28, 3,  22, 5,  4}, // 14 rounds
+                 {},
+                 {29, 4,  46, 61, 44, 11}}; // 16 rounds
+vi key_mask_14_mid_rounds{19, 35, 54, 32, 22, 0, 55, 41, 29, 9, 60, 42, 28,
+                          10}; //starts at 0, repeated keys: 9,
+
+vvi s1{{14, 4,  13, 1, 2,  15, 11, 8,  3,  10, 6,  12, 5,  9,  0, 7},
+       {0,  15, 7,  4, 14, 2,  13, 1,  10, 6,  12, 11, 9,  5,  3, 8},
+       {4,  1,  14, 8, 13, 6,  2,  11, 15, 12, 9,  7,  3,  10, 5, 0},
+       {15, 12, 8,  2, 4,  9,  1,  7,  5,  11, 3,  14, 10, 0,  6, 13}};
+
+vvi s5{{2,  12, 4,  1,  7,  10, 11, 6,  8,  5,  3,  15, 13, 0, 14, 9},
+       {14, 11, 2,  12, 4,  7,  13, 1,  5,  0,  15, 10, 3,  9, 8,  6},
+       {4,  2,  1,  11, 10, 13, 7,  8,  15, 9,  12, 5,  6,  3, 0,  14},
+       {11, 8,  12, 7,  1,  14, 2,  13, 6,  15, 0,  9,  10, 4, 5,  3}};
+/*
+
+int attackAlgorithm2FewLevels(int num_of_rounds, int num_of_inputs, vvvvd& pre_calculated_mat){
+    std::string binary_used_key;
+    createBinText(binary_used_key);
+    static int input_matrix[NUM_OF_FIRST_LAST_KEYS][matrix_size][matrix_size] = {0};
+    for(int i = 0; i < num_of_inputs; i ++){
+        std::pair<std::string, std::string> plain_cipher_pair = getPlainCipherPair(num_of_rounds,
+                                                                                   binary_used_key);
+        std::string plain_left = plain_cipher_pair.first.substr(0, 32);
+        std::string plain_right = plain_cipher_pair.first.substr(32, 32);
+        std::string cipher_left = plain_cipher_pair.second.substr(0, 32);
+        std::string cipher_right = plain_cipher_pair.second.substr(32, 32);
+
+        long long p_left = stoll(plain_left, nullptr, 2);
+        long long p_right = stoll(plain_right, nullptr, 2);
+        long long c_left = stoll(cipher_left, nullptr, 2);
+        long long c_right = stoll(cipher_right, nullptr, 2);
+
+        int s1_in = ((p_right&1) << 5) + (p_right >> 27); // bits 32,1,2,3,4,5 of p_right;
+        int s5_in = (c_right >> 11)&63; // bits 16,17,18,19,20, 21 of c_right;
+
+        int s1_out_first = calcOutSbox(p_left, s1_out_shift_mask);
+        int s5_out_first = calcOutSbox(p_right, s5_out_shift_mask); // char_plain_left
+        int s1_out_last = calcOutSbox(c_right, s1_out_shift_mask); // char_cipher_right (swap!!!)
+        int s5_out_last = calcOutSbox(c_left, s5_out_shift_mask);
+
+        for(int first_last_key = 0; first_last_key < NUM_OF_FIRST_LAST_KEYS; ++ first_last_key){
+            //std::pair<int, int> char_plain_cipher_pair = calcCharPlainCipher(first_round, last_round, first_last_key);
+
+            int first_key = (first_last_key&4032) >> 6; // Bin - 111111000000
+            int last_key = first_last_key&63; // Bin- 000000111111
+
+            int input_s1 = first_key^s1_in;
+            int input_s5 = last_key^s5_in;
+
+            int char_plain_right = s1_out_first^sboxFunction(1, input_s1);
+            int char_cipher_left = s5_out_last^sboxFunction(5, input_s5);
+
+            int char_plain = (s5_out_first << 4) + char_plain_right;
+            int char_cipher = (char_cipher_left << 4) + s1_out_last;
+
+            input_matrix[first_last_key][char_plain][char_cipher] ++;
+        }
+    }
+
+    int char_rounds = num_of_rounds - 2;
+    std::string str_curr_first_key = getSubInput(binary_used_key, first_mask);
+    std::string str_curr_last_key = getSubInput(binary_used_key, last_mask[num_of_rounds - 1]);
+    std::string str_curr_first_last_key = str_curr_first_key + str_curr_last_key;
+    std::string str_curr_middle_key = getSubInput(binary_used_key, key_mask_14_mid_rounds);
+    std::string str_curr_middle_key_by_rounds = str_curr_middle_key.substr(0, char_rounds);
+
+    int curr_first_last_key = stoi(str_curr_first_last_key, nullptr, 2);
+    int curr_middle_key_by_rounds = stoi(str_curr_middle_key_by_rounds, nullptr, 2);
+
+
+    int location = 1;
+    double used_key_distance = calculateDistance(curr_middle_key_by_rounds, char_rounds,
+                                                 num_of_inputs, input_matrix[curr_first_last_key],
+                                                 pre_calculated_mat);
+
+    for(int first_last_key = 0; first_last_key < NUM_OF_FIRST_LAST_KEYS; first_last_key ++){
+        for(int middle_key = 0; middle_key < pow(2, char_rounds); middle_key ++){
+            if( first_last_key == curr_first_last_key && middle_key == curr_middle_key_by_rounds ) continue;
+            double curr_dist = calculateDistance(middle_key, char_rounds, num_of_inputs,
+                                                 input_matrix[first_last_key], pre_calculated_mat);
+            if( curr_dist > used_key_distance ){
+                location ++;
+            }
+        }
+    }
+    return location;
+}
+*/
